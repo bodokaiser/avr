ebd65f32288a1d170a18bbe44422b05d76924e0f
Bodo Kaiser <i@bodokaiser.io>
2016-02-28 17:12:35 +0100

    updated led blink to use timer interrupt

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
-#define F_CPU 1000000UL
-
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 int main(void) {
+    /* use PA0, PB0, PD0 as output GPIO */
+    DDRA = (1 << PA0);
     DDRB = (1 << PB0);
+    DDRD = (1 << PD0);
 
-    while (1) {
-        PORTB ^= (1 << PB0);
+    /* set PA0 low, PB0 high */
+    PORTA &= (1 << PA0);
+    PORTB |= (1 << PB0);
 
-        _delay_ms(2000);
+    /* enable overflow interrupt */
+    TIMSK = (1 << TOIE0);
+    /* set prescale to 1024 */
+    TCCR0 = (1 << CS02) | (1 << CS00);
+    /* enable interrupts */
+    sei();
+
+    for (;;) {
+        /* do nothing */
     }
 
     return 0;
+}
+
+ISR (TIMER0_OVF_vect) {
+    /* toggle PD0 */
+    PORTD ^= (1 << PD0);
 }
